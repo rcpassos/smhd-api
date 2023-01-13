d7b6f2d45dc32fb51663acc78969b5a381b64c4f
Rafael Passos <r.passos@outlook.pt>
2023-01-13 22:39:18 +0000

    fix timestamps and response error

--- src/handlers/put-item.mjs
+++ src/handlers/put-item.mjs
@@ -42,7 +42,11 @@ export const putItemHandler = async (event) => {
   const uvIndex = body.uv_index;
   const temperature = body.temperature;
   const lightIntensity = body.light_intensity;
-  const happenedAt = body.happened_at;
+  let happenedAt = body.happened_at;
+
+  if (happenedAt) {
+    happenedAt = new Date(happenedAt).toISOString();
+  }
 
   // Creates a new item, or replaces an old item with a new item
   // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
@@ -60,20 +64,23 @@ export const putItemHandler = async (event) => {
       soil_moisture: soilMoisture,
       uv_index: uvIndex,
       happened_at: happenedAt,
+      created_at: new Date().toISOString(),
     },
+  };
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(body),
   };
 
   try {
     const data = await ddbDocClient.send(new PutCommand(params));
     console.log("Success - item added or updated", data);
   } catch (err) {
+    response.statusCode = 400;
+    response.body = (err.message || err.stack).toString();
     console.log("Error", err.stack);
   }
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(body),
-  };
 
   // All log statements are written to CloudWatch
   console.info(
